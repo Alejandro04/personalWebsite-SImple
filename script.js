9c255110a03221e7facd9208263477e2b15716ad
Alejandro Roa <alejodev04@gmail.com>
2020-04-21 10:54:55 -0400

    items

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let button = document.getElementById("icon")
 let links = document.getElementById("links")
 let svgHero = document.getElementById("svgHero") 
-let itemsButton = document.getElementsByClassName("skillsItem")
+let skillsItem = document.getElementById("skillsItem")
+let serviceItem = document.getElementById("serviceItem")
 let count = 0
 
 button.addEventListener("click", function(){
@@ -17,7 +18,20 @@ button.addEventListener("click", function(){
     }
 })
 
-itemsButton[0].addEventListener("click", function(){
+skillsItem.addEventListener("click", function(){
+    if(count===0){
+        links.className = ("links active")
+        svgHero.classList = ("svgHeroInactive")
+        count = 1
+    }else{
+        links.classList.remove("active")
+        links.className = ("links inactive")
+        svgHero.classList.remove("svgHeroInactive")
+        count = 0
+    }
+})
+
+serviceItem.addEventListener("click", function(){
     if(count===0){
         links.className = ("links active")
         svgHero.classList = ("svgHeroInactive")
