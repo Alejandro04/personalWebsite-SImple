b0c45c3a1e8e0d579b9fb3665c454b7769b90d67
Alejandro Roa <alejodev04@gmail.com>
2020-04-21 10:58:27 -0400

    contact

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let links = document.getElementById("links")
 let svgHero = document.getElementById("svgHero") 
 let skillsItem = document.getElementById("skillsItem")
 let serviceItem = document.getElementById("serviceItem")
+let contactItem = document.getElementById("contactItem")
 let count = 0
 
 button.addEventListener("click", function(){
@@ -32,6 +33,19 @@ skillsItem.addEventListener("click", function(){
 })
 
 serviceItem.addEventListener("click", function(){
+    if(count===0){
+        links.className = ("links active")
+        svgHero.classList = ("svgHeroInactive")
+        count = 1
+    }else{
+        links.classList.remove("active")
+        links.className = ("links inactive")
+        svgHero.classList.remove("svgHeroInactive")
+        count = 0
+    }
+})
+
+contactItem.addEventListener("click", function(){
     if(count===0){
         links.className = ("links active")
         svgHero.classList = ("svgHeroInactive")
