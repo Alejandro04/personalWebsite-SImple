f01540274ed0fe331545d5709fa34aa1342ae189
Alejandro Roa <alejodev04@gmail.com>
2020-04-22 15:01:18 -0400

    work

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let svgHero = document.getElementById("svgHero")
 let skillsItem = document.getElementById("skillsItem")
 let serviceItem = document.getElementById("serviceItem")
 let contactItem = document.getElementById("contactItem")
+let workItem = document.getElementById("workItem")
 let count = 0
 
 button.addEventListener("click", function(){
@@ -46,6 +47,19 @@ serviceItem.addEventListener("click", function(){
 })
 
 contactItem.addEventListener("click", function(){
+    if(count===0){
+        links.className = ("links active")
+        svgHero.classList = ("svgHeroInactive")
+        count = 1
+    }else{
+        links.classList.remove("active")
+        links.className = ("links inactive")
+        svgHero.classList.remove("svgHeroInactive")
+        count = 0
+    }
+})
+
+workItem.addEventListener("click", function(){
     if(count===0){
         links.className = ("links active")
         svgHero.classList = ("svgHeroInactive")
